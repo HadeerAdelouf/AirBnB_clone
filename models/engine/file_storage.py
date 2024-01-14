79fd838816423223a6319a7bc44f2a1aecebbc85
HadeerAdelouf <hadiradel999@gmail.com>
2024-01-14 21:13:14 +0200

    Airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,42 @@ class FileStorage:
                         FileStorage.__objects[key] = inst_ance
                 except Exception:
                     pass
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname"""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Review":
+                     {"place_id": str,
+                      "user_id": str,
+                      "text": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list}
+
+        }
+        return attributes
