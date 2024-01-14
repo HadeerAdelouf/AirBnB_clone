b4b4eebc43d7cb82b6955ac576991ce3bc2ade67
HadeerAdelouf <hadiradel999@gmail.com>
2024-01-14 22:10:58 +0200

    Airbnb

--- console.py
+++ console.py
@@ -3,59 +3,55 @@
 import cmd
 import sys
 import json
-import re
-import shlex
+import os
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-from models.amenity import Amenity
-from models.review import Review
 from models.place import Place
 from models.state import State
 from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Class for the command interpreter HBNBCommand """
+    """ General Class for HBNBCommand """
     prompt = '(hbnb) '
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.classes = storage.classes()
+    classes = {'BaseModel': BaseModel, 'User': User, 'City': City,
+               'Place': Place, 'Amenity': Amenity, 'Review': Review,
+               'State': State}
 
     def do_quit(self, arg):
-        """ Exit method """
+        """ Exit method for quit typing """
         exit()
-
-    def emptyline(self):
-        """ Method to pass when emptyline entered """
-        pass
 
     def do_EOF(self, arg):
         """ Exit method for EOF """
         print('')
         exit()
 
+    def emptyline(self):
+        """ Method to pass when emptyline entered """
+        pass
+
     def do_create(self, arg):
         """ Create a new instance """
         if len(arg) == 0:
             print('** class name missing **')
             return
-        new_model = None
+        new = None
         if arg:
             arg_list = arg.split()
             if len(arg_list) == 1:
                 if arg in self.classes.keys():
-                    new_model = self.classes[arg]()
-                    new_model.save()
-                    print(new_model.id)
+                    new = self.classes[arg]()
+                    new.save()
+                    print(new.id)
                 else:
                     print("** class doesn't exist **")
 
     def do_show(self, arg):
-        """
-        Prints the string representation of an instance.
-        """
+        """ Method to print instance """
         if len(arg) == 0:
             print('** class name missing **')
             return
@@ -73,23 +69,21 @@ class HBNBCommand(cmd.Cmd):
             print('** instance id missing **')
 
     def do_destroy(self, arg):
-        """
-        Deletes an instance based on the class name and id.
-        """
+        """ Method to delete instance with class and id """
         if len(arg) == 0:
             print("** class name missing **")
             return
-        line = arg.split()
+        arg_list = arg.split()
         try:
-            obj = eval(line[0])
+            obj = eval(arg_list[0])
         except Exception:
             print("** class doesn't exist **")
             return
-        if len(line) == 1:
+        if len(arg_list) == 1:
             print('** instance id missing **')
             return
-        if len(line) > 1:
-            key = line[0] + '.' + line[1]
+        if len(arg_list) > 1:
+            key = arg_list[0] + '.' + arg_list[1]
             if key in storage.all():
                 storage.all().pop(key)
                 storage.save()
@@ -98,76 +92,42 @@ class HBNBCommand(cmd.Cmd):
                 return
 
     def do_all(self, arg):
-        """
-        Print the string representation of all instances or a specific class.
-        """
-        if arg != "":
-            argss = arg.split(' ')
-            if argss[0] not in storage.classes():
-                print("** class doesn't exist **")
-            else:
-                nl = [str(obj) for key, obj in storage.all().items()
-                      if type(obj).__name__ == argss[0]]
-                print(nl)
+        """ Method to print all instances """
+        if len(arg) == 0:
+            print([str(a) for a in storage.all().values()])
+        elif arg not in self.classes:
+            print("** class doesn't exist **")
         else:
-            new_obj = [str(obj) for key, obj in storage.all().items()]
-            print(new_obj)
+            print([str(a) for b, a in storage.all().items() if arg in b])
 
     def do_update(self, arg):
-        """Updates an instance by adding or updating attribute.
-        """
-        if arg == "" or arg is None:
-            print("** class name missing **")
+        """ Method to update JSON file"""
+        arg = arg.split()
+        if len(arg) == 0:
+            print('** class name missing **')
             return
-
-        regex = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
-        match = re.search(regex, arg)
-        classname, uid, attribute, value = match.groups()
-        if not match:
-            print("** class name missing **")
-        elif classname not in storage.classes():
+        elif arg[0] not in self.classes:
             print("** class doesn't exist **")
-        elif uid is None:
-            print("** instance id missing **")
+            return
+        elif len(arg) == 1:
+            print('** instance id missing **')
+            return
         else:
-            key = "{}.{}".format(classname, uid)
-            if key not in storage.all():
-                print("** no instance found **")
-            elif not attribute:
-                print("** attribute name missing **")
-            elif not value:
-                print("** value missing **")
-            else:
-                cast = None
-                if not re.search('^".*"$', value):
-                    if '.' in value:
-                        cast = float
+            key = arg[0] + '.' + arg[1]
+            if key in storage.all():
+                if len(arg) > 2:
+                    if len(arg) == 3:
+                        print('** value missing **')
                     else:
-                        cast = int
+                        setattr(
+                            storage.all()[key],
+                            arg[2],
+                            arg[3][1:-1])
+                        storage.all()[key].save()
                 else:
-                    value = value.replace('"', '')
-                attr = storage.attributes()[classname]
-                if attribute in attr:
-                    value = attr[attribute](value)
-                elif cast:
-                    try:
-                        value = cast(value)
-                    except ValueError:
-                        pass
-                setattr(storage.all()[key], attribute, value)
-                storage.all()[key].save()
-
-    def do_count(self, line):
-        """Print the count all class instances"""
-        kclass = globals().get(line, None)
-        if kclass is None:
-            print("** class doesn't exist **")
-            return
-        count = 0
-        for obj in storage.all().values():
-            if obj.__class__.__name__ == line:
-                count += 1
-        print(count)
+                    print('** attribute name missing **')
+            else:
+                print('** no instance found **')
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,109 +1,45 @@
 #!/usr/bin/python3
-"""
-Module for FileStorage class 4 serializing and deserializing data
-"""
+""" class FileStorage
+    serializes instances to a JSON file
+    and deserializes JSON file to instances """
+import json
+import uuid
 import os
 from datetime import datetime
-import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
-    """
-    Class for serializtion and deserialization of base classes
-    """
+    """ construct """
     __file_path = "file.json"
     __objects = {}
 
-    def classes(self):
-        """Returns a dictionary of valid classes and their references"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
-
     def all(self):
-        """returns the dictionary objects"""
+        """ return dictionary objects """
         return FileStorage.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key
-        <obj class name>.id"""
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        """ sets in dictionary the obj with key <obj class name>.id """
+        FileStorage.__objects[obj.__class__.__name__ + "." + str(obj.id)] = obj
 
     def save(self):
-        """ serializes __objects to the JSON file"""
-        store_objs = FileStorage.__objects
-        obj_dictionary = {}
-
-        for obj in store_objs.keys():
-            obj_dictionary[obj] = store_objs[obj].to_dict()
-
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
-            json.dump(obj_dictionary, file)
+        """ serializes objectss to the JSON file (path: __file_path) """
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as fname:
+            new_dict = {key: obj.to_dict() for key, obj in
+                        FileStorage.__objects.items()}
+            json.dump(new_dict, fname)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects"""
-        if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
-                try:
-                    obj_dictionary = json.load(file)
-                    for key, value in obj_dictionary.items():
-                        class_name, obj_id = key.split('.')
-                        CC = eval(class_name)
-                        inst_ance = CC(**value)
-
-                        FileStorage.__objects[key] = inst_ance
-                except Exception:
-                    pass
-
-    def attributes(self):
-        """Returns the valid attributes and their types for classname"""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Review":
-                     {"place_id": str,
-                      "user_id": str,
-                      "text": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list}
-
-        }
-        return attributes
+        """ Reload the file """
+        if (os.path.isfile(FileStorage.__file_path)):
+            with open(FileStorage.__file_path, 'r', encoding="utf-8") as fname:
+                l_json = json.load(fname)
+                for key, val in l_json.items():
+                    FileStorage.__objects[key] = eval(
+                        val['__class__'])(**val)
